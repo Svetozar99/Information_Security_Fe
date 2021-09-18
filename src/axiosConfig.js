d7f0cab5c371b380cfd1829e2c1dea394c160633
Brboric Svetozar <svetozarbrboric@gmail.com>
2021-09-18 16:27:02 +0200

    https

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'http://localhost:8081',
+    baseURL:'https://localhost:8081',
+    https: true,
 });
 
 export default instance
---
+++ src/components/medicalRecord/MyMedicialRecord.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1></h1>
+</template>
+
+<script>
+import axios from '../../axiosConfig'
+
+export default {
+    name:'MyMedicialRecord',
+    created(){
+        let token = localStorage.getItem('token');
+        axios.defaults.headers['Authorization'] = `${token}`
+        this.getMyRecord()
+    },
+    data(){
+        return {
+            myrecord: {},
+            errorMsg: ''
+        }
+    },
+    methods:{
+        getMyRecord(){
+            axios
+            .get('api/medicalRecords/my-record')
+            .then((response) => {
+                this.myrecord = response.data
+                console.log(this.myrecord + ' myrecord')
+            })
+            .catch((error) => {
+                console.log(error)
+                this.errorMsg = 'Error retriving data'
+            })
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
